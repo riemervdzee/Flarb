ae8f5b3613002ce02fb9332897b9042ab019a476
Riemer van der Zee <riemervdzee@gmail.com>
2013-05-16 15:30:33 +0200

    Patching flarb_canbus to have a higher thoroughput

--- flarb_canbus/include/flarb_canbus/cController.h
+++ flarb_canbus/include/flarb_canbus/cController.h
@@ -6,21 +6,32 @@
 #include "flarb_canbus/cCanbus.h"
 #include "flarb_canbus/cRosCom.h"
 
+// HZ we need to check the status of the canbus
+#define CANBUS_HZ 3
+
 /*
  * Main controller class of the example node
  */
 class cController
 {
 public:
+	// C-tor
+	cController();
+
 	// Functions executed at the beginning and end of the Application
-	bool Create();
+	bool Create( int hz);
 	void Destroy();
 
 	// Updates the Node
 	void Update();
 
 private:
-	
+	// Controller_hz / canbus_hz = the amount we need to skip
+	int _hz;
+
+	// Current skipped controller_hz steps
+	int _skipped;
+
 	// Reference to the ros node handle
 	ros::NodeHandle _rosNode;
 
