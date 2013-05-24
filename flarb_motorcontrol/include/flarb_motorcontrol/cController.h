cd342be633ffc9887915b701061dd6293ec51bb4
Riemer van der Zee <riemervdzee@gmail.com>
2013-05-24 15:37:23 +0200

    Strip flarb_motorcontrol from the PID code, code class still exists though

--- flarb_motorcontrol/include/flarb_motorcontrol/cController.h
+++ flarb_motorcontrol/include/flarb_motorcontrol/cController.h
@@ -3,17 +3,13 @@
 
 #include "ros/ros.h"
 
-#include "flarb_motorcontrol/cPID.h"
 #include "flarb_motorcontrol/cRosCom.h"
 
 // The width of the chassis in meters
 #define CHASSIS_WIDTH 0.4f
-#define MOTOR_MAX     255
-
-#define PID_KP 0.9
-#define PID_KI 0.4
-#define PID_KD 0.0001
-// TODO fix D part of PID controller
+#define MOTOR_MAX     100
+#define VECTOR_MAX    0.5f
+#define VEC2MOTOR     (MOTOR_MAX / VECTOR_MAX)
 
 /*
  * Main controller class of the example node
@@ -21,9 +17,6 @@
 class cController
 {
 public:
-	// C-tor for default vals
-	cController() : _inputLeft(0.0f), _inputRight(0.0f), _outputLeft(0.0f), _outputRight(0.0f) {}
-
 	// Functions executed at the begining and end of the Application
 	bool Create( int hz);
 	void Destroy();
@@ -40,15 +33,6 @@ private:
 
 	// RosCom obj, deals with the communication towards other ROS nodes
 	cRosCom _roscom;
-
-	// Motor strengths as requested by /steering/waypoint/
-	float _inputLeft, _inputRight;
-
-	// Outputs calculated by the PID
-	float _outputLeft, _outputRight;
-
-	// PID controllers
-	cPID _pidLeft, _pidRight;
 };
 
 #endif // CLASS_CONTROLLER_H
--- flarb_motorcontrol/src/cController.cpp
+++ flarb_motorcontrol/src/cController.cpp
@@ -8,10 +8,6 @@ using namespace std;
 // Functions executed at the beginning and end of the Node
 bool cController::Create( int hz)
 {
-	// Set PIDs
-	_pidLeft  = cPID( hz, DIR_DIRECT, -MOTOR_MAX, MOTOR_MAX, PID_KP, PID_KI, PID_KD);
-	_pidRight = cPID( hz, DIR_DIRECT, -MOTOR_MAX, MOTOR_MAX, PID_KP, PID_KI, PID_KD);
-
 	// RosCom
 	_roscom.Create( &_rosNode, this);
 	return true;
@@ -26,24 +22,25 @@ void cController::Destroy()
 // Calculates motor strengths based on input
 void cController::SetWaypoint( float x, float y)
 {
-	// TODO scale motor strengths to wanted size
+	// Convert the vector to two motor strengths
+	// TODO write this a bit more proper. Support turning!
 	float AlphaRadians = atan2( y, x);
-	float SpeedFactor  = sqrt(x*x + y*y*4);
+	float SpeedFactor  = sqrt(x*x + y*y*4) * VEC2MOTOR;
 	float R       = AlphaRadians / M_PI;
 	float L	      = (AlphaRadians < 0) ? (-1 - R) : (1-R);
-	_inputRight   = R * SpeedFactor * 100;
-	_inputLeft    = L * SpeedFactor * 100;
+	_inputRight   = R * SpeedFactor;
+	_inputLeft    = L * SpeedFactor;
 
+	// Send motor strengths
+	_roscom.SendMotorStrength( (int)_inputLeft, (int)_inputRight);
+
+#if 0
 	cout << "[SET] Right " << _inputRight << ", Left " << _inputLeft << endl;
+#endif
 }
 
 // Updates the controller obj
 void cController::Update()
 {
-	// TODO PID control..
-	//_roscom.SendMotorStrength( (int)_inputLeft, (int)_inputRight);
-	_outputRight = _pidRight.Execute( _inputRight, _outputRight);
-	//_outputLeft  =  _pidLeft.Execute(  _inputLeft,  _outputLeft);
-
-	cout << "[UPD] Right " << _outputRight << endl;// ", Left " << _outputLeft  << endl;
+	// TODO after X updates with no SetWaypoint, brake the mofo thing
 }
