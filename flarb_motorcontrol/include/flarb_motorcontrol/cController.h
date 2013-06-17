c56ff96f09a904c78f0a8edb6f79ec56038ab99d
Riemer van der Zee <riemervdzee@gmail.com>
2013-06-17 22:51:46 +0200

    Reset timer

--- flarb_motorcontrol/include/flarb_motorcontrol/cController.h
+++ flarb_motorcontrol/include/flarb_motorcontrol/cController.h
@@ -10,7 +10,7 @@
 const float PULSES_PER_TURN = 50;               // TODO not known yet
 const float METERS_PER_TURN = 0.10f * 2 * M_PI; // Circumference of wheel
 const float VECTOR_MAX      = 0.5f;             // TODO decide
-const float VECTOR_TIME     = 4;                // The amount of sec, the input needs to be executed
+const float VECTOR_TIME     = 1;                // The amount of sec, the input needs to be executed
 
 // Converts
 const float PULSE2METER   = METERS_PER_TURN / PULSES_PER_TURN;
