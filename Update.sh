df8302964721312652e165e4843a18b7ea942250
Riemer van der Zee <riemervdzee@gmail.com>
2013-06-25 21:00:09 +0200

    Update script patch

--- Update.sh
+++ Update.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-rsync -r -v ./  nhl@flarb_main:~/groovy_workspace/flarb/
+#rsync -r -v ./  nhl@flarb_main:~/groovy_workspace/flarb/
+#scp -r ./flarb_canbus/bin/     nhl@flarb_main:~/groovy_workspace/flarb/flarb_canbus/
+#scp -r ./flarb_compass/bin/    nhl@flarb_main:~/groovy_workspace/flarb/flarb_compass/
+#scp -r ./flarb_controller/bin/ nhl@flarb_main:~/groovy_workspace/flarb/flarb_controller/
+
+#scp -r ./flarb_mapbuilder/bin/   nhl@flarb_main:~/groovy_workspace/flarb/flarb_mapbuilder/
+#scp -r ./flarb_motorcontrol/bin/ nhl@flarb_main:~/groovy_workspace/flarb/flarb_motorcontrol/
+#scp -r ./flarb_VDMixer/bin/      nhl@flarb_main:~/groovy_workspace/flarb/flarb_VDMixer/
